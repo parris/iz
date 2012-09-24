378644848f3f67619fa6642657889ab44f11af8c
Parris Khachi <parris.khachi@aeriagames.com>
2012-09-23 21:06:14 -0700

    added CC validation

--- spec/zuul.spec.js
+++ spec/zuul.spec.js
@@ -28,6 +28,46 @@ describe("Zuul", function() {
         zuul.between(function (){}, function (){}, function () {}).should.not.be.ok; //it also despises functions
     });
 
+    it("Can validate boolean values", function () {
+        zuul.boolean(true).should.be.ok;
+        zuul.boolean(false).should.be.ok;
+        zuul.boolean(1).should.be.ok;
+        zuul.boolean(0).should.be.ok;
+        zuul.boolean(-1).should.not.be.ok;
+        zuul.boolean("deadbeef").should.not.be.ok;
+        zuul.boolean("*").should.not.be.ok;
+        zuul.boolean(/[ -]/g).should.not.be.ok
+    });
+
+    /**
+     * Tests from Paypal: http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+     */
+    it("Can validate credit card numbers", function () {
+        zuul.cc("371449635398431").should.be.ok; //amex
+        zuul.cc("343434343434343").should.be.ok; //amex
+        zuul.cc("371144371144376").should.be.ok; //amex corp
+        zuul.cc("5610591081018250").should.be.ok; //aus bankcard
+        zuul.cc("30569309025904").should.be.ok; //diners club
+        zuul.cc("38520000023237").should.be.ok; //diners club
+        zuul.cc("6011111111111117").should.be.ok; //discover
+        zuul.cc("6011000990139424").should.be.ok; //discover
+        zuul.cc("3530111333300000").should.be.ok; //jcb
+        zuul.cc("3566002020360505").should.be.ok; //jcb
+        zuul.cc("5555555555554444").should.be.ok; //mc
+        zuul.cc("5105105105105100").should.be.ok; //mc
+        zuul.cc("4111111111111111").should.be.ok; //visa
+        zuul.cc("4012888888881881").should.be.ok; //visa
+        zuul.cc("4222222222222").should.be.ok; //visa
+        //zuul.cc("76009244561").should.be.ok; //dankort (pbs) currently fails... anyone know why?
+        zuul.cc("5019717010103742").should.be.ok; //dankort (pbs)
+        zuul.cc("6331101999990016").should.be.ok; //switch/solo (paymentech)
+
+        zuul.cc("0000000000000000").should.not.be.ok;
+
+        zuul.cc("4012 8888 8888 1881").should.be.ok; //visa with spaces
+        zuul.cc("4012-8888-8888-1881").should.be.ok; //visa with dashes
+    });
+
     xit("Can validate integers", function () {
         zuul.int(intTestPassString1).should.be.ok;
     });
--- zuul.js
+++ zuul.js
@@ -28,13 +28,47 @@ var zuul = zuul || {};
         return false;
     }
 
+    function zuul_boolean(value) {
+        if (typeof value === "boolean" || (typeof value === "number" && (value === 0 || value === 1))) {
+            return true;
+        }
+        return false;
+    }
+
     function zuul_int(value) {
         return (/^\s*(\+|-)?\d+\s*$/).test(value);
+    }
+
+    /**
+     * @author Phil Green (ShirtlessKirk) https://gist.github.com/2134376
+     */
+    function luhnChk(luhn) {
+        var len = luhn.length,
+            mul = 0,
+            prodArr = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]],
+            sum = 0;
+
+        while (len--) {
+            sum += prodArr[mul][parseInt(luhn.charAt(len), 10)];
+            mul ^= 1;
+        }
+
+        return sum % 10 === 0 && sum > 0;
+    }
+
+    function zuul_cc(value) {
+        value = value.replace(/[ -]/g, ""); // normalizing
+        if (zuul_int(value)) {
+            return luhnChk(value);
+        }
+        return false;
     }
 
     //This is just static, no need to make a new instance here
     zuul.alphaNumeric = zuul_alphaNumeric;
     zuul.between = zuul_between;
+    zuul.boolean = zuul_boolean;
+    zuul.cc = zuul_cc;
     zuul.int = zuul_int;
 }());
 
