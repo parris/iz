392a24d8b92847ee80aab9ef887eb0ffc40b2464
Parris Khachi <parris@letswoosh.com>
2013-04-06 22:59:03 -0700

    Fixed comment

--- src/web.js
+++ src/web.js
@@ -2,9 +2,7 @@
 (function() {
     'use strict';
 
-    // serves as our bootstrap into other node apps
-    // let's not clutter up the .bin folder
-    // this also helps when using onejs to compile for client side
+    // helps when using onejs to compile for client side
     var iz = require('./iz'),
         are = require('./are'),
         validators = require('./validators');
