805c39b14ffe74f52d272fa2315678ad3f6f9fcf
Parris Khachi <parris@letswoosh.com>
2013-08-28 00:23:30 -0700

    Added validFor and JSON based validations

--- spec/are.spec.js
+++ spec/are.spec.js
@@ -7,24 +7,120 @@ var iz = require("../src/iz"),
 describe("Are", function() {
     'use strict';
 
-    beforeEach(function() {
-        this.costErrors = {
-            decimal: "Must be a decimal!"
-        };
-        this.ageErrors = {
-            not_decimal: "Shouldn't be a decimal!",
-            int: "Must be an integer",
-            between: "This movie is rated R, you are too young!"
-        };
-        this.rules = {
-            cost: iz(5.00, this.costErrors).decimal(),
-            age: iz(20, this.ageErrors)
-                .not().decimal()
-                .int().between(17, 10000)
-        };
+    describe('group validations', function() {
+
+        beforeEach(function() {
+            this.costErrors = {
+                decimal: "Must be a decimal!"
+            };
+            this.ageErrors = {
+                not_decimal: "Shouldn't be a decimal!",
+                int: "Must be an integer",
+                between: "This movie is rated R, you are too young!"
+            };
+            this.rules = {
+                cost: iz(5.00, this.costErrors).number(),
+                age: iz(20, this.ageErrors)
+                    .not().decimal()
+                    .int().between(17, 10000)
+            };
+        });
+
+        it('allows you to validate multiple things at once', function () {
+            are(this.rules).valid().should.be.ok;
+        });
+
     });
 
-    it("allows you to validate multiple things at once", function () {
-        are(this.rules).valid().should.not.be.ok;
+    describe('JSON group validations', function() {
+
+        beforeEach(function() {
+            this.rules = {
+                'cost': [
+                    {
+                        'rule': 'between',
+                        'args': [17, 1000],
+                        'error': 'The cost must be between 17, 1000'
+                    },
+                    {
+                        'rule': 'decimal',
+                        'error': 'The cost must be a decimal'
+                    },
+                    {
+                        'rule': 'required',
+                        'error': 'You must specify a cost'
+                    },
+                ],
+                'producer.id': [
+                    {
+                        'rule': 'int',
+                        'error': 'Producer ID must be an int'
+                    }
+                ],
+                'producer.name.first': [
+                    {
+                        'rule': 'alphaNumeric',
+                        'error': 'Must be names and numbers'
+                    }
+                ]
+            };
+
+            this.validObject = {
+                cost: 90.3,
+                producer: {
+                    id: 5,
+                    name: {
+                        first: 'steve'
+                    }
+                }
+            };
+
+            this.invalidObject = {
+                cost: 90.3,
+                producer: {
+                    id: 'bob', //invalid
+                    name: {
+                        first: 'steve'
+                    }
+                }
+            };
+        });
+
+        it('correctly validates', function() {
+            are(this.rules).validFor(this.validObject).should.be.ok;
+            are(this.rules).validFor(this.invalidObject).should.not.be.ok;
+        });
+
+        describe('with required fields', function() {
+
+            beforeEach(function() {
+                this.rules['producer.name.first'].push({
+                    rule: 'required'
+                });
+
+                this.validObject = {
+                    cost: 40.3,
+                    producer: {
+                        name: {
+                            first: 'steve'
+                        }
+                    }
+                };
+
+                this.invalidObject = {
+                    producer: {}
+                };
+
+                this.validations = are(this.rules);
+            });
+
+            it('correctly validates', function() {
+                this.validations.validFor(this.validObject).should.be.ok;
+                this.validations.validFor(this.invalidObject).should.not.be.ok;
+            });
+
+        });
+
     });
+
 });
--- src/are.js
+++ src/are.js
@@ -1,17 +1,102 @@
-/*global module, exports, window*/
+/*global module, exports */
+
+var iz = require('./iz');
 
 (function () {
     'use strict';
     var are;
 
     function Are(rules) {
-        var self = this;
-        self._rules = rules;
+        var self = this,
+            currentRule,
+            rule,
+            errors,
+            key;
+
+        self._fields = {};
+
+        for (key in rules) {
+            if (!rules.hasOwnProperty(key)) {
+                continue;
+            }
+
+            // if is an iz object just add rule directly, if not assemble an iz object
+            if (typeof rules[key].revalidate !== 'undefined') {
+                self._fields[key] = rules[key];
+            } else {
+                errors = {};
+
+                // make errors dictionary
+                for (rule in rules[key]) {
+                    if (!rules[key].hasOwnProperty(rule)) {
+                        continue;
+                    }
+
+                    if (rules[key][rule].error) {
+                        errors[rules[key][rule].rule] = rules[key][rule].error;
+                    }
+                }
+
+                currentRule = iz(0, errors);
+
+                // call rule
+                for (rule in rules[key]) {
+                    if (!rules[key].hasOwnProperty(rule)) {
+                        continue;
+                    }
+
+                    // handle 'not_'
+                    if (rules[key][rule].rule.indexOf('not_') > -1) {
+                        currentRule.not();
+                    }
+
+                    currentRule[rules[key][rule].rule.replace('not_', '')].apply(
+                        currentRule,
+                        rules[key][rule].args || []
+                    );
+                }
+
+                self._fields[key] = currentRule;
+            }
+        }
 
         this.valid = function() {
-            for(var key in self._rules) {
-                self._rules[key].revalidate();
-                if (!self._rules[key].valid) {
+            for (var key in self._fields) {
+                if (!self._fields[key].hasOwnProperty(key)) {
+                    continue;
+                }
+
+                self._fields[key].revalidate();
+                if (!self._fields[key].valid) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        this.validFor = function(values) {
+            var field,
+                i = 0,
+                fieldKeys,
+                currentValue;
+
+            for (field in self._fields) {
+                if (!self._fields.hasOwnProperty(field)) {
+                    continue;
+                }
+
+                fieldKeys = field.split('.');
+                currentValue = values[fieldKeys[0]];
+
+                // account for chained field names
+                for (i = 1; i < fieldKeys.length; i++) {
+                    currentValue = currentValue[fieldKeys[i]];
+                }
+
+                self._fields[field].setValue(currentValue);
+
+                if (!self._fields[field].valid) {
                     return false;
                 }
             }
